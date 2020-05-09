5ea9e71f114de12149835e92e9f915279b2e26a1
Rodney <rolson@indeed.com>
2020-05-09 11:41:18 -0700

    Initial Commit

---
+++ First Watch App WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  First Watch App WatchKit Extension
+//
+//  Created by Rodney Olson on 5/9/20.
+//  Copyright © 2020 Rodney Olson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
